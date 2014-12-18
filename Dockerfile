3841d06663daaa3d511274beaf08f719662546cb
Shaun Mulligan <switchflip.shaun@gmail.com>
2014-12-18 14:49:46 +0200

    basic comments added

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ FROM resin/rpi-buildstep-armv6hf:latest
 RUN apt-get update
 RUN apt-get install -y python python-dev libraspberrypi-bin python-pip
 
+# Install picamera python module using pip
 RUN pip install picamera
 # add the root dir to the /app dir in the container env
 ADD . /app
--- demo.py
+++ demo.py
@@ -9,6 +9,9 @@ with picamera.PiCamera() as camera:
     time.sleep(2)
     camera.capture('../data/image.jpg')
 
+# this infinite loop just allows us to keep the container running
+# so we can use the webterminal to ssh in and check that the image
+# was saved in /data
 while 1:
 	print 'waiting...'
 	time.sleep(30)
